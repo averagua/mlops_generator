e639b44524985d60488c105043983cef090d2ca7
averagua <veragua.alb@gmail.com>
2020-12-13 19:18:52 -0300

    RC 1.0.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/averagua/mlops_generator",
-    version="0.1.1",
+    version="1.0.1",
     zip_safe=False,
 )
